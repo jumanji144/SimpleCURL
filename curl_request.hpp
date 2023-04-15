8be739401c7532ca3de5ed0ffef98d571e012a66
sightem <sightem@national.shitposting.agency>
2023-04-15 11:57:51 -0400

    Add download_file() method

--- curl_request.hpp
+++ curl_request.hpp
@@ -3,6 +3,7 @@
 #include <map>
 #include <sstream>
 #include <vector>
+#include <fstream>
 #ifdef MANUAL_CURL_PATH // manually linking
 #include MANUAL_CURL_PATH
 #else
@@ -93,6 +94,27 @@ public:
         this->prepare();
         curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, method.c_str());
         return this->execute();
+    }
+    /**
+     * @brief Download a file from the specified URL and save it to the given file path
+     * @param file_path The path where the downloaded file should be saved
+     * @return The result of the cURL request
+     */
+    int download_file(const std::string& file_path)
+    {
+        std::ofstream ofs(file_path, std::ios::out | std::ios::binary);
+
+        this->prepare();
+
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, file_write_callback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &ofs);
+        curl_easy_setopt(curl, CURLOPT_FAILONERROR, 1L);
+
+        int result = curl_easy_perform(curl);
+
+        ofs.close();
+
+        return result;
     }
     /**
      * @brief set the url of the request
@@ -291,6 +313,14 @@ private:
 
         response.curlCode = curlCode;
         return response;
+    }
+    
+    static size_t file_write_callback(void* ptr, size_t size, size_t nmemb, void* userdata)
+    {
+        std::ofstream* ofs = static_cast<std::ofstream*>(userdata);
+        size_t bytes_written = size * nmemb;
+        ofs->write(static_cast<const char*>(ptr), bytes_written);
+        return bytes_written;
     }
 
     CURL* curl;
